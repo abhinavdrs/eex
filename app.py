7a867d825a994c3fbebf73671233b23c698faabe
Abhinav Mishra <abhinav.mishra@mailbox.tu-dresden.de>
2022-03-31 17:23:02 +0200

    send_report_by_email

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Run this app to generate reports
 from margin_checker.logic import MarginChecker
 from data import CI050, CC050
 
+
 def compare():
     """
     Initialize a margin_checker object to generate and send report via email.
@@ -15,9 +16,11 @@ def compare():
     reporting_date = '2020-05-12'
     compare_obj = MarginChecker(reporting_date, CC050, CI050)
     compare_obj.assess_report()
+    compare_obj.send_report_by_email()
 
     # generate individual reports to .csv
     compare_obj.write_assessed_report_to_csv()
+
 
 if __name__ == "__main__":
     compare()
